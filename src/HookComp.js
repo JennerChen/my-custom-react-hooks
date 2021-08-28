7ff9996bf4446feb4cf5dd4aeb460e2167bc39ee
zhangqing <zhangqing332@live.com>
2021-08-28 18:30:10 +0800

    feat: 自定义render实现hooks

---
+++ src/HookComp.js
@@ -0,0 +1,11 @@
+import React, { useState } from "react";
+
+export default function (){
+    const [count , setCount] = useState(0);
+
+    return <div>
+        <button onClick={ () => setCount(count+1) }>+</button>
+        count: {count} <span>{count}</span>
+        <button onClick={ () => setCount(count-1) }>-</button>
+    </div>
+}
